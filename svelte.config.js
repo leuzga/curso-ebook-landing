2bc55b66abf956102b90908f353984e5c54b9387
Lenin Uzcategui <c-luzcategu@cdsreg.com>
2025-02-15 12:17:40 -0300

    ebook 3th sections

---
+++ svelte.config.js
@@ -0,0 +1,20 @@
+import adapter from '@sveltejs/adapter-netlify';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter(),
+		alias: {
+			$lib: './src/lib',
+			$components: './src/lib/components',
+			$assets: './src/assets', //$assets: '../src/assets'
+		}
+	}
+};
+
+export default config;
